d07d8340683c83066b2f5031d2a0d8c9ea040076
Chris Green <chrisgreen1993@gmail.com>
2024-03-25 09:57:23 +0000

    Add Reddit module for fetching new posts

---
+++ lib/listen_list/reddit.ex
@@ -0,0 +1,49 @@
+defmodule ListenList.Reddit do
+  @moduledoc """
+  Module for fetching new posts from the subreddit /r/indieheads.
+  """
+
+  alias HTTPoison
+
+
+  @post_data_key_map %{
+    "id" => "external_id",
+    "title" => "title",
+    "url" => "url",
+    "score" => "score",
+    "permalink" => "permalink"
+  }
+
+  @query_params [
+    q: "self:no[FRESH ALBUM]",
+    restrict_sr: "on",
+    sort: "new",
+    limit: 5,
+    t: "month"
+  ]
+
+
+  def fetch_new_releases do
+    search_url = "https://www.reddit.com/r/indieheads/search.json?" <> URI.encode_query(@query_params)
+
+    case HTTPoison.get(search_url) do
+      {:ok, %{status_code: 200, body: body}} ->
+        {:ok, %{"data" => %{"children" => posts}}} = Jason.decode(body)
+        Enum.map(posts, &parse_post/1)
+
+      {:ok, %{status_code: status_code}} ->
+        {:error, "HTTP request failed with status code #{status_code}"}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp parse_post(%{"data" => data}) do
+    data
+    |> Map.take(Map.keys(@post_data_key_map))
+    |> Enum.map(fn {k, v} -> {String.to_atom(Map.get(@post_data_key_map, k)), v} end)
+    |> Enum.into(%{})
+    |> Map.put(:raw_post, data)
+  end
+end
