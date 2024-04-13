35cf59425e9cd8bbdf0dcf703ebe48fd5362a62f
Chris Green <chrisgreen1993@gmail.com>
2024-04-13 15:13:59 +0000

    Mix task to fetch new releases manually
    A bit easier than having to drop into iex

---
+++ lib/mix/tasks/fetch_releases.ex
@@ -0,0 +1,9 @@
+defmodule Mix.Tasks.FetchReleases do
+  use Mix.Task
+
+  @shortdoc "Manually fetch new releases from Reddit and store them in the database"
+  def run(_args) do
+    {:ok, _} = Application.ensure_all_started(:listen_list)
+    ListenList.Jobs.FetchReleasesJob.fetch_releases()
+  end
+end
