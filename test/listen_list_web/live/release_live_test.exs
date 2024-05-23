d2d8be5e96b28d226b4b3145d1635fb2a768bfed
Chris Green <chrisgreen1993@gmail.com>
2024-05-23 05:29:25 +0000

    fix test

--- test/listen_list_web/live/release_live_test.exs
+++ test/listen_list_web/live/release_live_test.exs
@@ -15,7 +15,7 @@ defmodule ListenListWeb.ReleaseLiveTest do
     test "lists all releases", %{conn: conn, release: release} do
       {:ok, _index_live, html} = live(conn, ~p"/")
 
-      assert html =~ "ListenList"
+      assert html =~ "Listen List"
     end
   end
 end
