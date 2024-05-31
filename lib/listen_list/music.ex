e2ec0c99627df0cd3e4d41faa2ee5d4e2effaf8c
Chris Green <chrisgreen1993@gmail.com>
2024-05-31 14:06:31 +0000

    Move create_or_update macro to a separate macros module
    It's probably not a good idea having in the music module
    as macros must be defined before they are used, so reordering the
    Music module could cause it to break

--- lib/listen_list/music.ex
+++ lib/listen_list/music.ex
@@ -8,7 +8,9 @@ defmodule ListenList.Music do
 
   alias ListenList.Music.Release
   alias ListenList.Utils.Time
+  alias ListenList.Music.Macros
 
+  require ListenList.Music.Macros
   require Logger
 
   @doc """
@@ -154,43 +156,6 @@ defmodule ListenList.Music do
     Release.changeset(release, attrs)
   end
 
-  # Macro to generate the ON CONFLICT clause for the release import
-  # The update: parameter doesn't let us to build fragments dynamically,
-  # so instead we use a macro do do it.
-  # this generates fragments that update all fields except for import_status, artist and album
-  # if import_status has a value of 'manual' or 'rejected' we keep the existing value
-  defmacro create_or_update_conflict_action() do
-    # We do not want to update the id or inserted_at fields
-    fields = Release.__schema__(:fields) -- [:id, :inserted_at]
-
-    set_list =
-      Enum.map(fields, fn field ->
-        case field do
-          field when field in [:artist, :album, :import_status] ->
-            # for these fields we generate an SQL case statement to check the existing value in import_status
-            quote do
-              {unquote(field),
-               fragment(
-                 "CASE WHEN ? IN ('manual', 'rejected') THEN ? ELSE excluded.? END",
-                 field(r, :import_status),
-                 field(r, ^unquote(field)),
-                 literal(^Atom.to_string(unquote(field)))
-               )}
-            end
-
-          _ ->
-            # the rest of the fields we just update with the new value
-            quote do
-              {unquote(field), fragment("excluded.?", literal(^Atom.to_string(unquote(field))))}
-            end
-        end
-      end)
-
-    quote do
-      from(r in Release, update: [set: unquote(set_list)])
-    end
-  end
-
   # Create or update releases
   # insert_all only takes maps so we need to validate changesets manually
   def create_or_update_releases(releases) do
@@ -214,7 +179,7 @@ defmodule ListenList.Music do
       |> Enum.reject(&is_nil/1)
 
     Repo.insert_all(Release, valid_releases,
-      on_conflict: create_or_update_conflict_action(),
+      on_conflict: Macros.create_or_update_conflict_action(),
       conflict_target: :reddit_id
     )
   end
---
+++ lib/listen_list/music/macros.ex
@@ -0,0 +1,40 @@
+defmodule ListenList.Music.Macros do
+  alias ListenList.Music.Release
+
+  # Macro to generate the ON CONFLICT clause for the release import
+  # The update: parameter doesn't let us to build fragments dynamically,
+  # so instead we use a macro do do it.
+  # this generates fragments that update all fields except for import_status, artist and album
+  # if import_status has a value of 'manual' or 'rejected' we keep the existing value
+  defmacro create_or_update_conflict_action() do
+    # We do not want to update the id or inserted_at fields
+    fields = Release.__schema__(:fields) -- [:id, :inserted_at]
+
+    set_list =
+      Enum.map(fields, fn field ->
+        case field do
+          field when field in [:artist, :album, :import_status] ->
+            # for these fields we generate an SQL case statement to check the existing value in import_status
+            quote do
+              {unquote(field),
+               fragment(
+                 "CASE WHEN ? IN ('manual', 'rejected') THEN ? ELSE excluded.? END",
+                 field(r, :import_status),
+                 field(r, ^unquote(field)),
+                 literal(^Atom.to_string(unquote(field)))
+               )}
+            end
+
+          _ ->
+            # the rest of the fields we just update with the new value
+            quote do
+              {unquote(field), fragment("excluded.?", literal(^Atom.to_string(unquote(field))))}
+            end
+        end
+      end)
+
+    quote do
+      from(r in Release, update: [set: unquote(set_list)])
+    end
+  end
+end
